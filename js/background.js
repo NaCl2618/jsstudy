01f2289f627333fe2bc7fed1344cad32cca455a1
NaCl2618 <52007247+NaCl2618@users.noreply.github.com>
2021-11-28 15:44:05 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ console.log("loading background.js");
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(../img/${chosenImage})`;
+document.body.style.backgroundImage = `url(../jsstudy/img/${chosenImage})`;
 
 
 
